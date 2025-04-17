a4162cc88388f19a5f570559984c4d71a7b30932
Aman kumar Prajapati <amanprajapati7266@gmail.com>
2025-04-17 12:44:46 +0530

    connect MySql

---
+++ Projects/Aman/frontend/src/RegistrationForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './RegistrationForm.css'; // import CSS
+
+function RegistrationForm() {
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+
+  const handleChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:5000/register', form);
+      alert(res.data.msg);
+    } catch (err) {
+      alert("Registration failed: " + err.response?.data?.msg || err.message);
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="register-form">
+        <h2>Register</h2>
+        <input name="name" placeholder="Name" onChange={handleChange} required />
+        <input name="email" placeholder="Email" type="email" onChange={handleChange} required />
+        <input name="password" placeholder="Password" type="password" onChange={handleChange} required />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegistrationForm;
